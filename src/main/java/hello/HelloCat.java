78fbd94dd66cf4b335bafd3c2f3ebf975498a721
a.korobitsyn <andrii.korobitsyn@xrm.ru>
2017-03-27 13:27:58 +0300

    build script is fixed, extra files are added to gitignore and deleted

--- src/main/java/hello/HelloCat.java
+++ src/main/java/hello/HelloCat.java
@@ -22,15 +22,15 @@ public class HelloCat {
     EntityManager entityManager = emf.createEntityManager();
     DbHelper.createTestDb(entityManager);
 
-    likeExample(entityManager);
-    nestedObjFieldLikeExample(entityManager);
+    simpleLike(entityManager);
+    nestedObjFieldEquals(entityManager);
 
 
     entityManager.close();
     emf.close();
   }
 
-  private static void likeExample (EntityManager em){
+  private static void simpleLike (EntityManager em){
     System.out.println("############################## Cat names like: %ge%");
     JPAQuery queryFactory = new JPAQuery(em);
     QCat cat = QCat.cat;
@@ -41,17 +41,26 @@ public class HelloCat {
     catNames.forEach(System.out::println);
   }
 
-  private static void nestedObjFieldLikeExample (EntityManager em){
-    System.out.println("############################## Owner names equals(QueryDsl): Bill");
+  private static void nestedObjFieldEquals (EntityManager em){
 
+    System.out.println("############################## Owner names equals(JPQL): Bill");
+    TypedQuery<Cat> queryJpql = em.createQuery(
+            "SELECT c FROM Cat c JOIN c.owner o WHERE o.name=:ownerName", Cat.class);
+    queryJpql.setParameter("ownerName", "Bill");
+    List<Cat> catsJpql = queryJpql.getResultList();
+    catsJpql.forEach(System.out::println);
+
+
+    System.out.println("############################## Owner names equals(QueryDsl): Bill");
     JPAQuery queryFactory = new JPAQuery(em);
     QCat cat = QCat.cat;
     QOwner owner = QOwner.owner;
-    List<Cat> catNames = queryFactory.from(cat)
-            .leftJoin(cat.owner, owner)
+    List<Cat> catsQdsl = queryFactory.from(cat)
+            .join(cat.owner, owner)
             .where(owner.name.eq("Bill"))
             .list(cat);
-    catNames.forEach(System.out::println);
+    catsQdsl.forEach(System.out::println);
+
 
     System.out.println("############################## Owner names equals(CriteriaQuery): Bill");
     CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -59,9 +68,9 @@ public class HelloCat {
     Root<Cat> catQ = criteriaQuery.from(Cat.class);
     Join<Cat, Owner> ownerQ = catQ.join(Cat_.owner);
     criteriaQuery.where(cb.equal(ownerQ.get(Owner_.name), "Bill"));
-    Query query = em.createQuery(criteriaQuery);
-    List<Cat> catNamesQ = query.getResultList();
-    catNamesQ.forEach(System.out::println);
+    TypedQuery<Cat> query = em.createQuery(criteriaQuery);
+    List<Cat> catsCrQ = query.getResultList();
+    catsCrQ.forEach(System.out::println);
   }
 
 
